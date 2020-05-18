d174c0af41aab2cb8a44563dd6df8d7145287eb0
Stephen Bergeron <bergeron.stephen@gmail.com>
2020-05-18 16:10:21 -0400

    Add tc utility to the mix. 	modified:   secure.bash

--- bin/secure.bash
+++ bin/secure.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#sudo apt install openssh-server
 #cat /etc/os-release
 #sudo ufw status verbose
 
-sudo apt update
-sudo apt full-upgrade
+sudo apt -y update
+sudo apt -y full-upgrade
+sudo apt -y install openssh-server
+
+#https://www.cybertec-postgresql.com/en/postgresql-network-latency-does-make-a-big-difference/
+tc -g -s class show dev eth0
+
